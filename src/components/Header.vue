c1352aaea89c287ffb2bab225169c0cf06c124dc
Victoria Cheng <victoria.cheng15@gmail.com>
2024-02-25 11:28:05 -0700

    chore: add header, link container and footer

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import avatar from "/avatar.jpg"
+const { name, headline } = defineProps<{ name: string; headline: string }>();
+const imageInfo = {
+  src: avatar,
+  alt: name,
+  width: 200,
+  height: 200,
+}
+</script>
+
+<template>
+  <header>
+    <div>
+      <img :="imageInfo" />
+      <h1>{{ name }}</h1>
+      <p>{{ headline }}</p>
+    </div>
+  </header>
+</template>
