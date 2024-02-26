0cf0284d4a596b355abc1271b2a42b365b556049
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-02-26 09:20:25 -0700

    chore: add overall style for the page (#3)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import { css } from "../styled-system/css"
 import { Header, LinkContainer, Footer } from "./components"
 import { NAME, HEADLINE } from "./constant"
+
+
 </script>
 
 <template>
-  <Header :name="NAME" :headline="HEADLINE" />
-  <LinkContainer />
-  <Footer :name="NAME" />
+  <div :class="css({
+    height: '100vh',
+    maxW: '2xl',
+    marginInline: 'auto',
+    paddingTop: '12',
+    paddingInline: '8',
+    display: 'flex',
+    flexDir: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  })">
+    <Header :name="NAME" :headline="HEADLINE" />
+    <LinkContainer />
+    <Footer :name="NAME" />
+  </div>
 </template>
