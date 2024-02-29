f18ab26b6f98507e7bb6b175e7d1808c5de483a2
Victoria Cheng <victoria.cheng15@gmail.com>
2024-02-29 11:01:50 -0700

    chore: add social element component and responsive for the list

---
+++ src/components/SocialElement.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { href, path, altText } = defineProps<{ href: string; path?: string, altText: string }>();
+</script>
+
+<template>
+  <a :href="href" target="_blank" rel="noopener noreferrer">
+    <img :src="path" :alt="altText" width="45" height="45" />
+  </a>
+</template>
