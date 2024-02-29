3324f6368cb86febd75fae9eb69e4e762c1bf17a
Victoria Cheng <victoria.cheng15@gmail.com>
2024-02-29 11:13:18 -0700

    chore: add link element component

--- src/components/LinkContainer.vue
+++ src/components/LinkContainer.vue
@@ -1,31 +1,22 @@
 <script setup lang="ts">
 import { css } from "../../styled-system/css"
 import { LINKS } from "../constant";
+import LinkElement from "./LinkElement.vue";
 
 const styles = css({
   w: '100%',
   h: '100%',
-  bg: 'gray.900',
-  p: '4',
-  fontSize: '2xl',
-  fontWeight: 'bold',
-  letterSpacing: 'widest',
-  textAlign: 'center',
-  rounded: 'lg',
-  transition: 'ease-in-out',
-  transitionDuration: '400ms',
-  _hover: { bg: 'blue.400', color: 'gray.900', transition: 'ease-in-out', transitionDuration: '400ms' }
+  display: 'grid',
+  placeItems: 'center'
 })
+
 </script>
 
 <template>
   <main :class="css({ w: '100%', mb: '14' })">
     <ul :class="css({ display: 'grid', gap: '8' })">
-      <li v-for="{ title, href } in LINKS" :key="title"
-        :class="css({ w: '100%', h: '100%', display: 'grid', placeItems: 'center' })">
-        <a :href="href" target="_blank" rel="noopener noreferrer" :class="styles">
-          {{ title }}
-        </a>
+      <li v-for="{ title, href } in LINKS" :key="title" :class="styles">
+        <LinkElement :href="href" :title="title" />
       </li>
     </ul>
   </main>
