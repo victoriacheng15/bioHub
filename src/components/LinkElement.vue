dc048d0f6f663f49d2eaf42e220544fd5b5320d8
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-02-29 11:14:40 -0700

    chore: add link element component (#9)

---
+++ src/components/LinkElement.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { css } from "../../styled-system/css"
+const { href, title } = defineProps<{ href: string, title: string }>()
+
+const styles = css({
+  w: '100%',
+  h: '100%',
+  bg: 'gray.900',
+  p: '4',
+  fontSize: { base: 'xl', md: '2xl' },
+  fontWeight: 'semibold',
+  letterSpacing: 'widest',
+  textAlign: 'center',
+  rounded: 'lg',
+  transition: 'ease-in-out',
+  transitionDuration: '400ms',
+  _hover: { bg: 'blue.400', color: 'gray.900', transition: 'ease-in-out', transitionDuration: '400ms' }
+})
+</script>
+
+<template>
+  <a :href="href" target="_blank" rel="noopener noreferrer" :class="styles">
+    {{ title }}
+  </a>
+</template>
