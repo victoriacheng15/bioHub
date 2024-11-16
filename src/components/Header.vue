b8bae4ffe6515443af0558fae8daf2458df2b775
Victoria Cheng <victoria.cheng15@gmail.com>
2024-11-16 15:09:30 -0700

    feat: remove panda css related and add reset css (#24)
    * feat: remove panda css related and add reset css
    
    * chore: remove panda css package
    
    * chore: remove panda css package
    
    * feat: add css style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { css } from "../../styled-system/css"
 import SocialList from "./SocialList.vue";
 import avatar from "/avatar.jpg"
 
-const FIXEDSIZE = 200
+const FIXEDSIZE = 175
 
 const { name, headline } = defineProps<{ name: string; headline: string }>();
 const imageInfo = {
@@ -15,16 +14,36 @@ const imageInfo = {
 </script>
 
 <template>
-  <header :class="css({ w: '100%', mb: { base: '8', md: '12' } })">
-    <div :class="css({ display: 'grid', gap: '4', mb: '8' })">
-      <img :="imageInfo" :class="css({ mx: 'auto', rounded: '50%' })" />
-      <h1 :class="css({ textAlign: 'center', fontSize: '4xl', fontWeight: 'semibold', letterSpacing: 'wide' })">{{ name
-      }}
-      </h1>
-      <p
-        :class="css({ textAlign: 'center', fontSize: { base: 'xl', sm: '2xl' }, transition: 'ease-in-out', transitionDuration: '400ms' })">
-        {{ headline }}</p>
-    </div>
+  <header>
+    <img :="imageInfo" />
+    <h1>{{ name }} </h1>
+    <p> {{ headline }}</p>
     <SocialList />
   </header>
 </template>
+
+<style scoped>
+header {
+  margin-top: 4rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.25rem;
+  text-align: center;
+}
+
+header h1 {
+  font-size: 2.5rem;
+  color: #ffa375;
+  letter-spacing: 2px;
+}
+
+header p {
+  font-size: 1.25rem;
+}
+
+img {
+  display: block;
+  border-radius: 100%;
+}
+</style>
--- src/components/SocialList.vue
+++ src/components/SocialList.vue
@@ -1,23 +1,34 @@
 <script setup lang="ts">
-import { css } from "../../styled-system/css"
 import { SCOIALS } from "../constant";
 import SocialElement from "./SocialElement.vue"
-
-const styles = css({
-  m: '1',
-  p: '1.5',
-  bg: 'blue.400',
-  rounded: 'md',
-  transition: 'ease-in-out',
-  transitionDuration: '400ms',
-  _hover: { scale: '1.25', transition: 'ease-in-out', transitionDuration: '400ms' }
-})
 </script>
 
 <template>
-  <ul :class="css({ display: 'flex', justifyContent: 'center', gap: { base: '4', sm: '6' }, flexWrap: 'wrap' })">
-    <li v-for="{ title, path, href } in  SCOIALS " :key="title" :class="styles">
+  <ul>
+    <li v-for="{ title, path, href } in SCOIALS " :key="title">
       <SocialElement :href="href" :path="path" :altText="title" />
     </li>
   </ul>
 </template>
+
+<style scoped>
+ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 1.25rem;
+}
+
+ul li {
+  padding: 0.25rem;
+  background: #1147bb;
+  border-radius: 5px;
+  transition: padding 500ms ease-in-out;
+}
+
+ul li:hover {
+  padding-inline: 0.75rem;
+
+}
+</style>
